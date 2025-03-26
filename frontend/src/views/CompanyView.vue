a7b22bc069411201eb13286b4ae36d2936e25dc7
Hanna Raudsepp <hrauds@taltech.ee>
2025-03-26 22:27:39 +0200

    refactor: update company data fields for shareholders in company details view

--- frontend/src/views/CompanyView.vue
+++ frontend/src/views/CompanyView.vue
@@ -20,7 +20,6 @@
         <div class="shadow-sm rounded bg-white p-4">
           <div class="mb-4">
             <h2 class="h5 mb-3">Põhiandmed</h2>
-
             <dl class="row">
               <dt class="col-sm-2">Nimi:</dt>
               <dd class="col-sm-9">{{ company.name }}</dd>
@@ -56,27 +55,27 @@
                 <tbody>
                   <tr v-for="(shareholder, index) in shareholders" :key="index">
                     <td>
-                      <span v-if="shareholder.type === 'individual'">
-                        {{ shareholder.first_name }} {{ shareholder.last_name }}
+                      <span v-if="shareholder.person.type === 'individual'">
+                        {{ shareholder.person.first_name }} {{ shareholder.person.last_name }}
                       </span>
                       <span v-else>
-                        {{ shareholder.legal_name }}
+                        {{ shareholder.person.legal_name }}
                       </span>
                     </td>
                     <td>
-                      {{ shareholder.type === 'individual' ? 'Füüsiline isik' : 'Juriidiline isik' }}
+                      {{ shareholder.person.type === 'individual' ? 'Füüsiline isik' : 'Juriidiline isik' }}
                     </td>
                     <td>
-                      <span v-if="shareholder.type === 'individual'">
-                        {{ shareholder.id_code }}
+                      <span v-if="shareholder.person.type === 'individual'">
+                        {{ shareholder.person.id_code }}
                       </span>
                       <span v-else>
-                        {{ shareholder.reg_code }}
+                        {{ shareholder.person.reg_code }}
                       </span>
                     </td>
                     <td>{{ shareholder.share }} €</td>
                     <td>{{ calculatePercentage(shareholder.share, company.capital) }}%</td>
-                    <td>{{ shareholder.isFounder ? 'Jah' : 'Ei' }}</td>
+                    <td>{{ shareholder.is_founder ? 'Jah' : 'Ei' }}</td>
                   </tr>
                 </tbody>
               </table>
@@ -129,26 +128,10 @@ export default {
           this.error = 'Osaühingut ei leitud'
           return
         }
-
         const apiBaseUrl = process.env.VUE_APP_API_URL || ''
-
-        const companyResponse = await axios.get(`${apiBaseUrl}/companies/${companyId}`)
-        this.company = companyResponse.data
-
-        const shareholdingsResponse = await axios.get(`${apiBaseUrl}/shareholdings/`, {
-          params: { company_id: companyId }
-        })
-
-        this.shareholders = await Promise.all(
-          shareholdingsResponse.data.map(async (shareholding) => {
-            const personResponse = await axios.get(`${apiBaseUrl}/persons/${shareholding.person_id}`)
-            return {
-              ...personResponse.data,
-              share: shareholding.share,
-              isFounder: shareholding.isFounder || false
-            }
-          })
-        )
+        const response = await axios.get(`${apiBaseUrl}/companies/${companyId}`)
+        this.company = response.data
+        this.shareholders = response.data.shareholders || []
       } catch (error) {
         console.error('Error fetching company data:', error)
         this.error = 'Viga andmete laadimisel: ' + (error.response?.data?.detail || error.message)
